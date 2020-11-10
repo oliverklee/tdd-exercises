f7bca47d9ec35b1e63972753a9021a705a3410af
Oliver Klee <github@oliverklee.de>
2020-11-10 18:19:05 +0100

    [FEATURE] Add an RLE encoder/decoder (#38)

---
+++ src/Compression/RunLengthEncoder.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace OliverKlee\TddSeed\Compression;
+
+use InvalidArgumentException;
+
+/**
+ * Compressor that uses run-length encoding.
+ */
+class RunLengthEncoder implements CompressorInterface
+{
+    /**
+     * marker used both to terminate the algorithm code at the start of the compressed data
+     * as well as to mark a sequence in the compressed data
+     *
+     * @var string
+     */
+    public const MARKER = '@';
+
+    /**
+     * @var int
+     */
+    private const MINIMUM_CHUNK_LENGTH = 3;
+
+    public function getAlgorithmCode(): string
+    {
+        return 'RLE';
+    }
+
+    public function compress(string $uncompressedData): string
+    {
+        $result = $this->getAlgorithmCode() . self::MARKER;
+
+        /** @var string[] $chunks */
+        $chunks = [];
+        $previousByte = '';
+        $currentChunk = '';
+        $inputBytes = str_split($uncompressedData);
+        foreach ($inputBytes as $byte) {
+            if ($byte === $previousByte) {
+                $currentChunk .= $byte;
+                continue;
+            }
+
+            if ($currentChunk !== '') {
+                $chunks[] = $currentChunk;
+            }
+            $previousByte = $byte;
+            $currentChunk = $byte;
+        }
+        if ($currentChunk !== '') {
+            $chunks[] = $currentChunk;
+        }
+
+        foreach ($chunks as $chunk) {
+            $chunkLength = strlen($chunk);
+            if ($chunkLength < self::MINIMUM_CHUNK_LENGTH) {
+                $result .= $chunk;
+            } else {
+                $result .= self::MARKER . chr($chunkLength) . $chunk{0};
+            }
+        }
+
+        return $result;
+    }
+
+    public function decompress(string $compressedData): string
+    {
+        $expectedPrefix = $this->getAlgorithmCode() . self::MARKER;
+        $prefixLength = strlen($expectedPrefix);
+        $actualPrefix = substr($compressedData, 0, $prefixLength);
+        if ($actualPrefix !== $expectedPrefix) {
+            throw new InvalidArgumentException('Missing or incorrect algorithm code', 1605025867);
+        }
+
+        $payload = substr($compressedData, $prefixLength);
+
+        $uncompressedData = '';
+
+        $payloadLength = strlen($payload);
+        $position = 0;
+        while ($position < $payloadLength) {
+            $currentByte = $payload{$position};
+            if ($currentByte !== self::MARKER) {
+                $uncompressedData .= $currentByte;
+                $position++;
+            } else {
+                $repetitions = ord($payload{$position + 1});
+                $repeatable = $payload{$position + 2};
+                $uncompressedData .= str_repeat($repeatable, $repetitions);
+                $position += 3;
+            }
+        }
+
+        return $uncompressedData;
+    }
+}
---
+++ tests/Unit/Compression/RunLengthEncoderTest.php
@@ -0,0 +1,185 @@
+<?php
+declare(strict_types=1);
+
+namespace OliverKlee\TddSeed\Tests\Unit\Compression;
+
+use OliverKlee\TddSeed\Compression\CompressorInterface;
+use OliverKlee\TddSeed\Compression\RunLengthEncoder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \OliverKlee\TddSeed\Compression\RunLengthEncoder
+ */
+final class RunLengthEncoderTest extends TestCase
+{
+    /**
+     * @var RunLengthEncoder
+     */
+    private $subject = null;
+
+    protected function setUp(): void
+    {
+        $this->subject = new RunLengthEncoder();
+    }
+
+    /**
+     * @test
+     */
+    public function isCompressor(): void
+    {
+        self::assertInstanceOf(CompressorInterface::class, $this->subject);
+    }
+
+    /**
+     * @test
+     */
+    public function getAlgorithmCodeReturnsNonEmptyString(): void
+    {
+        self::assertNotSame('', $this->subject->getAlgorithmCode());
+    }
+
+    /**
+     * @test
+     */
+    public function compressWithEmptyDataReturnsAlgorithmCodePlusMarker(): void
+    {
+        $result = $this->subject->compress('');
+
+        $code = $this->subject->getAlgorithmCode();
+        self::assertSame($code . RunLengthEncoder::MARKER, $result);
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider singleBytesDataProvider
+     */
+    public function compressWithSingleBytesReturnsDataStartingWithPrefix(string $singleBytes): void
+    {
+        $result = $this->subject->compress($singleBytes);
+
+        self::assertStringStartsWith($this->subject->getAlgorithmCode() . RunLengthEncoder::MARKER, $result);
+    }
+
+    /**
+     * @return array<string, array<string>>
+     */
+    public function singleBytesDataProvider(): array
+    {
+        return [
+            'one character' => ['a'],
+            'two different characters' => ['ab'],
+            'three different characters' => ['abc'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider singleBytesDataProvider
+     */
+    public function compressWithSingleBytesReturnsProvidedSingleBytesAfterPrefix(string $singleBytes): void
+    {
+        $result = $this->subject->compress($singleBytes);
+
+        self::assertSame($singleBytes, substr($result, $this->getPrefixLength()));
+    }
+
+    private function getPrefixLength(): int
+    {
+        return strlen($this->subject->getAlgorithmCode()) + 1;
+    }
+
+    /**
+     * @test
+     */
+    public function compressWithTwoIdenticalSubsequentBytesReturnsThemUncompressed(): void
+    {
+        $data = 'xx';
+        $result = $this->subject->compress($data);
+
+        self::assertSame($data, substr($result, $this->getPrefixLength()));
+    }
+
+    /**
+     * @test
+     * @dataProvider subsequentIdenticalBytesDataProvider
+     */
+    public function compressCompressesIdenticalSubsequentBytesWithCount(string $data): void
+    {
+        $result = $this->subject->compress($data);
+        $marker = $result{$this->getPrefixLength() - 1};
+
+        $payload = substr($result, $this->getPrefixLength());
+        $expectedPayload = $marker . chr(strlen($data)) . $data{0};
+        self::assertSame($expectedPayload, $payload);
+    }
+
+    /**
+     * @return array<string, array<string>>
+     */
+    public function subsequentIdenticalBytesDataProvider(): array
+    {
+        return [
+            '3 bytes' => ['bbb'],
+            '4 bytes' => ['bbbb'],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function decompressForIncorrectAlgorithmCodeThrowsException(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Missing or incorrect algorithm code');
+        $this->expectExceptionCode(1605025867);
+
+        $this->subject->decompress('What is love?');
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider singleBytesDataProvider
+     * @dataProvider subsequentIdenticalBytesDataProvider
+     * @dataProvider moreComplexPayloadDataProvider
+     */
+    public function compressReducesDataSizeAfterPrefix(string $uncompressed): void
+    {
+        $uncompressed = $this->subject->compress($uncompressed);
+
+        $uncompressedSize = strlen($uncompressed);
+        $compressedPayloadSize = strlen($uncompressed) - $this->getPrefixLength();
+        self::assertLessThan($uncompressedSize, $compressedPayloadSize);
+    }
+
+    /**
+     * @return array<string, array<string>>
+     */
+    public function moreComplexPayloadDataProvider(): array
+    {
+        return [
+            'empty string' => [''],
+            'multiple two-character sequences' => ['aabbccdd'],
+            'multiple three-character sequences' => ['aaabbbcccddd'],
+            'everything mixed' => ['aaabhhhsseffff4444'],
+            'one long chain of x' => ['xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider singleBytesDataProvider
+     * @dataProvider subsequentIdenticalBytesDataProvider
+     * @dataProvider moreComplexPayloadDataProvider
+     */
+    public function compressAndDecompressReturnsTheGivenData(string $originalData): void
+    {
+        $compressed = $this->subject->compress($originalData);
+        $decompressed = $this->subject->decompress($compressed);
+
+        self::assertSame($originalData, $decompressed);
+    }
+}
